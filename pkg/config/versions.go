341eaee874ebe73266854eab6a8df47a30203167
Ahmad Ibrahim <ahm.ibr@hotmail.com>
2024-07-23 17:25:02 -0700

    chore: make reviewable

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,7 +2,7 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.0.48",
+	Validator:              "v0.0.49",
 	ValidatorPluginAws:     "v0.1.1",
 	ValidatorPluginAzure:   "v0.0.13",
 	ValidatorPluginNetwork: "v0.0.19",
