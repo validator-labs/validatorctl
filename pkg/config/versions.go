65c49a4e9034771786dc3fa8858b10060ce35c03
Tyler Gillson <tyler.gillson@gmail.com>
2024-08-06 09:34:00 -0600

    chore: make reviewable
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,10 +2,10 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.0",
-	ValidatorPluginAws:     "v0.1.2",
-	ValidatorPluginAzure:   "v0.0.14",
-	ValidatorPluginNetwork: "v0.0.21",
-	ValidatorPluginOci:     "v0.0.12",
-	ValidatorPluginVsphere: "v0.0.28",
+	Validator:              "v0.1.1",
+	ValidatorPluginAws:     "v0.1.3",
+	ValidatorPluginAzure:   "v0.0.15",
+	ValidatorPluginNetwork: "v0.0.22",
+	ValidatorPluginOci:     "v0.0.13",
+	ValidatorPluginVsphere: "v0.0.29",
 }
