5d569deb02bf2a442ae62e912cdff5416f379a9f
Artur Shad Nik <37195151+arturshadnik@users.noreply.github.com>
2024-08-15 12:07:21 -0700

    fix: export creds for aws and azure direct check (#167)
    ## Issue
    Resolves #164 
    
    ## Description
    - In direct mode, AWS and Azure creds will now be exported as env
    variables before validator is invoked
    - In direct mode, user will no longer be prompted for `Implicit Auth`

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,7 +2,7 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.4",
+	Validator:              "v0.1.5",
 	ValidatorPluginAws:     "v0.1.4",
 	ValidatorPluginAzure:   "v0.0.16",
 	ValidatorPluginMaas:    "v0.0.7",
