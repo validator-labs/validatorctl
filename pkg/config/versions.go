b3316f01424123668c0145bda25733dea66f7785
Ahmad Malik Ibrahim <ahmad.ibrahim@spectrocloud.com>
2024-08-22 14:29:22 -0700

    fix: support `validator rules check -f config.yaml` without all plugins defined (#191)
    Resolves https://github.com/validator-labs/validatorctl/issues/188
    
    Ensures we dont panic when executing `validator rules check -f
    config.yaml` without all plugins defined

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -5,7 +5,7 @@ var ValidatorChartVersions = map[string]string{
 	Validator:              "v0.1.8",
 	ValidatorPluginAws:     "v0.1.5",
 	ValidatorPluginAzure:   "v0.0.18",
-	ValidatorPluginMaas:    "v0.0.9",
+	ValidatorPluginMaas:    "v0.0.8",
 	ValidatorPluginNetwork: "v0.0.24",
 	ValidatorPluginOci:     "v0.3.0",
 	ValidatorPluginVsphere: "v0.0.32",
