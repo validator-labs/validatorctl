013a23974798d51caa92ca2fe7f8075b5e2ee914
Artur Shad Nik <arturshadnik@gmail.com>
2024-08-22 19:45:42 -0700

    chore: make reviewable
    chore: make reviewable

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,11 +2,11 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.7",
-	ValidatorPluginAws:     "v0.1.4",
-	ValidatorPluginAzure:   "v0.0.17",
-	ValidatorPluginMaas:    "v0.0.7",
-	ValidatorPluginNetwork: "v0.0.23",
-	ValidatorPluginOci:     "v0.2.1",
-	ValidatorPluginVsphere: "v0.0.31",
+	Validator:              "v0.1.8",
+	ValidatorPluginAws:     "v0.1.6",
+	ValidatorPluginAzure:   "v0.0.19",
+	ValidatorPluginMaas:    "v0.0.8",
+	ValidatorPluginNetwork: "v0.0.25",
+	ValidatorPluginOci:     "v0.3.1",
+	ValidatorPluginVsphere: "v0.0.33",
 }
