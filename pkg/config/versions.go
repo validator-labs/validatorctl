4d70cfe45aeca4512d97a4f68789783d789e17d4
Matt Welke <matt.welke@spectrocloud.com>
2024-08-24 19:50:33 -0400

    Upgrade to latest version of each plugin (for YAML rendering fix).
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -3,10 +3,10 @@ package config
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
 	Validator:              "v0.1.8",
-	ValidatorPluginAws:     "v0.1.6",
-	ValidatorPluginAzure:   "v0.0.19",
-	ValidatorPluginMaas:    "v0.0.8",
-	ValidatorPluginNetwork: "v0.0.25",
-	ValidatorPluginOci:     "v0.3.1",
-	ValidatorPluginVsphere: "v0.0.33",
+	ValidatorPluginAws:     "v0.1.7",
+	ValidatorPluginAzure:   "v0.0.20",
+	ValidatorPluginMaas:    "v0.0.11",
+	ValidatorPluginNetwork: "v0.0.26",
+	ValidatorPluginOci:     "v0.3.2",
+	ValidatorPluginVsphere: "v0.0.34",
 }
