a936c652459537c460395d8ae9b65bd36ced7c18
Matt Welke <matt.welke@spectrocloud.com>
2024-09-05 13:16:39 -0400

    make reviewable
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,11 +2,11 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.9",
+	Validator:              "v0.1.10",
 	ValidatorPluginAws:     "v0.1.7",
 	ValidatorPluginAzure:   "v0.0.20",
 	ValidatorPluginMaas:    "v0.0.12",
-	ValidatorPluginNetwork: "v0.0.26",
+	ValidatorPluginNetwork: "v0.1.0",
 	ValidatorPluginOci:     "v0.3.3",
 	ValidatorPluginVsphere: "v0.0.34",
 }
