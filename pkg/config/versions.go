cc2dbc40f83603ab0f8f3266ca94dec620f15c6e
Ahmad Malik Ibrahim <ahmad.ibrahim@spectrocloud.com>
2024-10-03 17:44:46 -0700

    chore: bump validator and vsphere plugin version (#231)

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,11 +2,11 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.11",
+	Validator:              "v0.1.12",
 	ValidatorPluginAws:     "v0.1.7",
 	ValidatorPluginAzure:   "v0.0.21",
 	ValidatorPluginMaas:    "v0.0.12",
 	ValidatorPluginNetwork: "v0.1.0",
 	ValidatorPluginOci:     "v0.3.3",
-	ValidatorPluginVsphere: "v0.1.3",
+	ValidatorPluginVsphere: "v0.1.4",
 }
