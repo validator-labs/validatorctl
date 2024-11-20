ecc03b7e9902f920441aeb92c07a2f696743b007
Matt Welke <matt.welke@spectrocloud.com>
2024-11-20 16:40:53 -0500

    Update plugin versions.
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -3,7 +3,7 @@ package config
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
 	Validator:              "v0.1.12",
-	ValidatorPluginAws:     "v0.1.7",
+	ValidatorPluginAws:     "v0.1.8",
 	ValidatorPluginAzure:   "v0.0.22",
 	ValidatorPluginMaas:    "v0.0.12",
 	ValidatorPluginNetwork: "v0.1.0",
