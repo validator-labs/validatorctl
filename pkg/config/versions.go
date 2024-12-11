483f47f9134b063e6fca35a9519e15fc1b576d70
Artur Shad Nik <arturshadnik@gmail.com>
2024-12-11 13:35:54 -0800

    chore: update plugin versions, charts
    Signed-off-by: Artur Shad Nik <arturshadnik@gmail.com>

--- pkg/config/versions.go
+++ pkg/config/versions.go
@@ -2,11 +2,11 @@ package config
 
 // ValidatorChartVersions is a map of validator component names to their respective versions
 var ValidatorChartVersions = map[string]string{
-	Validator:              "v0.1.13",
-	ValidatorPluginAws:     "v0.1.10",
-	ValidatorPluginAzure:   "v0.0.24",
-	ValidatorPluginMaas:    "v0.0.12",
-	ValidatorPluginNetwork: "v0.1.0",
-	ValidatorPluginOci:     "v0.3.3",
-	ValidatorPluginVsphere: "v0.1.4",
+	Validator:              "v0.1.15",
+	ValidatorPluginAws:     "v0.1.11",
+	ValidatorPluginAzure:   "v0.0.25",
+	ValidatorPluginMaas:    "v0.0.13",
+	ValidatorPluginNetwork: "v0.1.1",
+	ValidatorPluginOci:     "v0.3.4",
+	ValidatorPluginVsphere: "v0.1.5",
 }
